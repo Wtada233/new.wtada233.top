39bf3becb8e82c598e0eefd51b1654933ca07513
Wtada233 <wtada233@outlook.com>
2025-11-30 20:14:58 +0800

    Update profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -1,19 +1,19 @@
 import type { ProfileConfig } from "../types/config";
 
 export const profileConfig: ProfileConfig = {
-	avatar: "/assets/images/avatar.webp",
-	name: "Firefly",
-	bio: "Hello, I'm Firefly.",
+	avatar: "/assets/images/demo-avatar.png",
+	name: "Wtada233",
+	bio: "我不生产屎山，我只是屎山的搬运工😀",
 	links: [
 		{
 			name: "Bilibli",
 			icon: "fa6-brands:bilibili",
-			url: "https://space.bilibili.com/38932988",
+			url: "https://space.bilibili.com/28700427",
 		},
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/Wtada233",
 		},
 	],
 };
